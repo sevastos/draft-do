4dd302d4430318fd42b251435a36674ac960ec3d
Sev <git@sevastos.com>
2014-02-09 09:11:58 +0200

    hbase zookeeper

--- hbase/hbase-conf.sh
+++ hbase/hbase-conf.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 iface=lo`uname | sed -n s/Darwin/0/p`
-cat > conf/hbase-site.xml <<EOF
+localip=`ip addr show eth0 | grep 'inet ' | cut -d/ -f1 | awk '{print $2}'`
+cat > ${HBASE_ROOT}/conf/hbase-site.xml <<EOF
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
   <property>
     <name>hbase.rootdir</name>
     <value>file:///$HBASE_DATA</value>
+  </property>
+  <property>
+    <name>hbase.zookeeper.quorum</name>
+    <value>$localip</value>
+  </property>
+  <property>
+    <name>hbase.zookeeper.property.dataDir</name>
+    <value>/data/zookeeper</value>
   </property>
   <property>
     <name>hbase.zookeeper.dns.interface</name>
@@ -18,8 +27,18 @@ cat > conf/hbase-site.xml <<EOF
     <value>$iface</value>
   </property>
   <property>
+    <name>hbase.master.ipc.address</name>
+    <value>$localip</value>
+  </property>
+  <property>
+    <name>hbase.regionserver.ipc.address</name>
+    <value>$localip</value>
+  </property>
+  <property>
     <name>hbase.master.dns.interface</name>
     <value>$iface</value>
   </property>
 </configuration>
 EOF
+
+echo "export HBASE_MANAGES_ZK=true" >> ${HBASE_ROOT}/conf/hbase-env.sh
